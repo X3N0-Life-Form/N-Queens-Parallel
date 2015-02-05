77f15b39bb9bd2d6fa0da3332aa43f4c6e72c2fd
X3N0-Life-Form <adrarsis@laposte.net>
2015-02-05 13:37:38 +0100

    getting prepared for paradigm shift

--- omp/n-queens-omp.cpp
+++ omp/n-queens-omp.cpp
@@ -4,15 +4,16 @@
 
 using namespace std;
 
-int* queens;
+int* global_queens;
 int size = 8;
 
-void printBoard() {
+
+void printBoard(int* queens) {
   for (int z = 0; z < size; z++) cout << queens[z] << " ";
   cout << endl;
 }
 
-void generateBoard() {
+void generateBoard(int* queens) {
   omp_set_num_threads(size);
   cout << "Generating board of size=" << size << endl;
 #pragma omp parallel
@@ -25,99 +26,79 @@ void generateBoard() {
 /**
  * @return true if the specified queen doesn't have any conflict.
  */
-bool checkQueen(int q, int* conflict) {
-  omp_set_num_threads(size);
-  bool ret = true;
-#pragma omp parallel
+bool checkQueen(int q, int* queens) {
+  for (int i = 0; i < size; i++)
   {
-    int i = omp_get_thread_num();
     if (i != q) {
       if (queens[i] == queens[q]) {
-	ret = false;
-	*conflict = i;
+	return false;
       }
 
       int diff = abs(i - q);
       if (abs(queens[i] - queens[q]) == diff) {
-	ret = false;
-	*conflict = i;
+	return false;
       }
     
-    }
-  }
-  return ret;
-}
-
-/**
- * @return true if the entire board is OK.
- */
-bool checkBoard(int* conflicts) {
-  for (int i = 0; i < size; i ++) {
-    int c;
-    if (!checkQueen(i, &c)) {
-      conflicts[0] = i;
-      conflicts[1] = c;
-     return false;
     }
   }
   return true;
 }
 
-void shuffleBoard() {
+/**
+ * @return true if the entire board is OK.
+ */
+bool checkBoard(int* queens) {
+  for (int i = 0; i < size; i ++) {
+    if (!checkQueen(i, queens)) {
+      return false;
+    }
+  }
+  return true;
+}
+
+void shuffleBoard(int* queens) {
   omp_set_num_threads(size);
 #pragma omp parallel
+  //for (int i = 0; i < size; i++)
   {
     int i = omp_get_thread_num();
     queens[i] = rand() % size;
   }
 }
 
-void shuffleQueen(int q) {
+void shuffleQueen(int q, int* queens) {
   queens[q] = rand() % size;
 }
 
-void swap(int q1, int q2) {
+void swap(int q1, int q2, int* queens) {
   int tmp = queens[q1];
   queens[q1] = queens[q2];
   queens[q2] = tmp;
 }
 
 int main(int argc, char** argv) {
+  srand(1);
   if (argc > 1) {
     size = atoi(argv[1]);
   }
-  queens = new int[size];
-  
-  generateBoard();
-  printBoard();
+  global_queens = new int[size];
+  generateBoard(global_queens);
+  printBoard(global_queens);
+
 
   clock_t begin, end;
   double time;
   begin = clock();
 
-  int* conflicts = new int[2];
-  while (!checkBoard(conflicts)) {
-    cout << "Conflict! ==> " << conflicts[0] << " & " << conflicts[1];
-    int diff = abs(queens[conflicts[0]] - queens[conflicts[1]]);
-    if (diff > 1) {
-      cout << "\tswap!";
-      int r = rand() % size;
-      while (r == conflicts[0]) r = rand() % size;
-      swap(conflicts[0], r);
-    } else {
-      cout << "\tshuffle!";
-      shuffleQueen(conflicts[0]);
-    }
-    cout << endl;
-    printBoard();
+  for (int i = 0; i < 10000; i++) {
+    
   }
   end = clock();
 
   time = (double) (end - begin) / CLOCKS_PER_SEC;
-  cout << "Final board: ";
-  printBoard();
+  //cout << "Final board with cost=" << cost << ": ";
+  printBoard(global_queens);
   cout << "Runtime: " << time << " s" << endl;
 
-  delete[] conflicts;
-  delete[] queens;
+  delete[] global_queens;
 }
